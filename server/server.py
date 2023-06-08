342e41465964817b153aaee000a5081054cd3ae0
Art-mj1 <mjmjpc@naver.com>
2023-06-08 22:55:27 +0900

    올리겠습니다 6월 8일

--- server/server.py
+++ server/server.py
@@ -5,3 +5,16 @@ root = Blueprint('root', __name__)
 @root.route('/',methods=['GET'])
 def main():
     return render_template('index.html')
+
+@root.route('/baskin/main',methods=['GET'])
+def gameMainGetMethod():
+    return render_template('./baskin/main/main.html')
+@root.route('/baskin/gamestart',methods=['GET'])
+def gameStartGetMethod():
+    return render_template('./baskin/gamestart/start.html')
+@root.route('/baskin/exp',methods=['GET'])
+def gameExpGetMethod():
+    return render_template('./baskin/exp/explanation.html')
+@root.route('/baskin/gameover',methods=['GET'])
+def gameOverGetMethod():
+    return render_template('./baskin/end/fail.html')
---
+++ templates/baskin/gamestart/start.js
@@ -0,0 +1,4 @@
+let btn = document.querySelector(".submit");
+btn.addEventListener("click", function () {
+  alert("제출되었습니다");
+});
