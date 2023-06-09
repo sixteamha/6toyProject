3270fa8823b468ab5d95e6b411f3798581b8d848
Art-mj1 <mjmjpc@naver.com>
2023-06-09 11:26:18 +0900

    6월 9일 게임 완료

--- templates/baskin/gamestart/start.js
+++
@@ -1,4 +0,0 @@
-let btn = document.querySelector(".submit");
-btn.addEventListener("click", function () {
-  alert("제출되었습니다");
-});
---
+++ templates/baskin/main/main.js
@@ -0,0 +1,15 @@
+function main() {
+  location.href = "../main/main.html";
+}
+function othergame() {
+  location.href = "/";
+}
+function explain() {
+  location.href = "../exp/explain.html";
+}
+function fail() {
+  location.href = "../end/fail.html";
+}
+function start() {
+  location.href = "../gamestart/start.html";
+}
